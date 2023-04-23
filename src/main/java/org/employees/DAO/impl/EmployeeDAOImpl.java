8a6fd117796121736817f181d78018629dcee973
Максим <gmax7911@gmail.com>
2023-04-23 14:58:16 +0300

    Задание выполнено

--- src/main/java/org/employees/DAO/impl/EmployeeDAOImpl.java
+++ src/main/java/org/employees/DAO/impl/EmployeeDAOImpl.java
@@ -1,11 +1,11 @@
 package org.employees.DAO.impl;
 
-import org.employees.City;
 import org.employees.DAO.EmployeeDAO;
 import org.employees.Employee;
+import org.employees.HibernateSessionFactoryUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDAOImpl implements EmployeeDAO {
@@ -16,124 +16,43 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public void create(Employee employee) {
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement statement = connection.prepareStatement(
-                "INSERT INTO employee (first_name, last_name, gender, age, city_id) VALUES ((?), (?), (?), (?), (?),)")) {
 
-            // Подставляем значение вместо wildcard
-            // первым параметром указываем порядковый номер wildcard
-            // вторым параметром передаем значение
-            statement.setString(1, employee.getFirstName());
-            statement.setString(2, employee.getLastName());
-            statement.setString(3, employee.getGender());
-            statement.setInt(4, employee.getAge());
-            statement.setInt(5, employee.getCity().getCityID());
-
-
-            // С помощью метода executeQuery отправляем запрос к базе
-            statement.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(employee);
+            transaction.commit();
         }
     }
 
 
     @Override
     public Employee readById(int id) {
-        Employee employee = new Employee();
-        // Формируем запрос к базе с помощью PreparedStatement
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM employee INNER JOIN city ON employee.city_id=city.city_id AND id=(?)")) {
-
-            // Подставляем значение вместо wildcard
-            statement.setInt(1, id);
-
-            // Делаем запрос к базе и результат кладем в ResultSet
-            ResultSet resultSet = statement.executeQuery();
-
-            // Методом next проверяем есть ли следующий элемент в resultSet
-            // и одновременно переходим к нему, если таковой есть
-            while (resultSet.next()) {
-
-                // С помощью методов getInt и getString получаем данные из resultSet
-                // и присваиваем их полим объекта
-                employee.setId(Integer.getInteger(resultSet.getString("id")));
-                employee.setFirstName(resultSet.getString("first_name"));
-                employee.setLastName(resultSet.getString("last_name"));
-                employee.setGender(resultSet.getString("gender"));
-                employee.setAge(resultSet.getInt("age"));
-                employee.setCity(new City(resultSet.getInt("city_id"),
-                        resultSet.getString("city_name")));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return employee;
+        return HibernateSessionFactoryUtil.getSessionFactory().
+                openSession().get(Employee.class, id);
     }
 
     @Override
     public List<Employee> readAll() {
-
-
-        // Создаем список, в который будем укладывать объекты
-        List<Employee> employeeList = new ArrayList<>();
-
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM employee INNER JOIN city ON employee.city_id=city.city_id")) {
-
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-
-                int id = Integer.getInteger(resultSet.getString("id"));
-                String firstName = resultSet.getString("first_name");
-                String lastname = resultSet.getString("last_name");
-                String gender = resultSet.getString("gender");
-                int age = resultSet.getInt("age");
-                City city = new City(resultSet.getInt("city_id"),
-                        resultSet.getString("city_name"));
-
-
-                // Создаем объекты на основе полученных данных
-                // и укладываем их в итоговый список
-                employeeList.add(new Employee(id, firstName, lastname, gender, age, city));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return employeeList;
+        List<Employee> employees = (List<Employee>) HibernateSessionFactoryUtil.getSessionFactory().openSession().createQuery("from Employee").list();
+        return employees;
     }
 
     @Override
-    public void updateEmployeeById(int id, int cityID) {
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement statement = connection.prepareStatement(
-                "UPDATE employee SET city_id=(?) WHERE id=(?)")) {
-
-            statement.setInt(1, cityID);
-            statement.setInt(2, id);
-
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public void updateAmount(Employee employee) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.update(employee);
+            transaction.commit();
         }
     }
 
 
     @Override
-    public void deleteById(int id) {
-        try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement statement = connection.prepareStatement(
-                "DELETE FROM employee WHERE id=(?)")) {
-
-            statement.setInt(1, id);
-            statement.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public void delete(Employee employee) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(employee);
+            transaction.commit();
         }
     }
 }
